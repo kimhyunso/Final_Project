bd3ff462dfae1c77f29ed38cb9f2d05924ec4451
kimhyunso <gustn9515@gmail.com>
2023-04-24 09:58:59 +0900

    20230424 09:58

--- base/final_project/views.py
+++ base/final_project/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index:test')
+    context = {}
+
+    return render(request, 'templates/index.html', context)
+
 
 
 
