78fe0c59429d0140325e743a33f966e36db56476
kimhyunso <gustn9515@gmail.com>
2023-04-25 10:51:01 +0900

    20230425 10:50

--- bert_project/main/admin.py
+++ bert_project/main/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import POST
-
-admin.site.register(POST)
--- bert_project/main/info.py
+++ bert_project/main/info.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import time
 import numpy as np
@@ -10,7 +11,10 @@ def search(keyword):
     keys = []
     columns = ['title', 'link', 'image', 'price', 'maker', 'category1', 'category2']
 
-    with open('API_KEY.txt', 'r') as key:
+    scriptpath = os.path.dirname(__file__)
+    filename = os.path.join(scriptpath, 'API_KEY.txt')
+    
+    with open(filename, 'r', encoding='utf-8') as key:
         for line in key.readlines():
             text = line.strip()
             if text != '':
@@ -24,7 +28,7 @@ def search(keyword):
     item_list = []
     columns = ['title', 'link', 'image', 'price', 'maker', 'category1', 'category2']
 
-    url = f'https://openapi.naver.com/v1/search/shop?&query={keyword}'
+    url = f'https://openapi.naver.com/v1/search/shop?&query={keyword}&display=8'
     time.sleep(3.2)
 
     response = requests.get(url, headers=headers)
