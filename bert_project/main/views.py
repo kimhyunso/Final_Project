61e1f408d870ee47aed2164265716fbdf37b9a6a
kimhyunso <gustn9515@gmail.com>
2023-04-25 17:49:59 +0900

    20230425 17:50

--- bert_project/main/views.py
+++ bert_project/main/views.py
@@ -20,6 +20,8 @@ def detail(request, keyword):
     }
     return render(request, 'main/detail.html', context)
 
+
+
 def page_not_found_view(request, exception):
     return render(request, '404.html', status=404)
 
@@ -29,24 +31,31 @@ def info(request):
     keyword = request.POST['keyword']
     info_df = search(keyword)
     conn = sqlite3.connect('db.sqlite3')
-
-    
-
     try:
         cursor = conn.cursor()
-        sql = 'INSERT INTO main_productinfo(title, link, imageURL, price, maker, category1, category2) VALUES(?, ?, ?, ?, ?, ?, ?);'
-        
-        rows = []
-        for idx, row in info_df.iterrows():
-            rows.append(row)
+        sql = f"SELECT * FROM main_productinfo WHERE category1={keyword};"
 
-        cursor.executemany(sql, rows)
-        conn.commit()
-        conn.close()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        if result != None:
+            return redirect('main:detail', keyword)
+        
+        else:
+            sql = "INSERT INTO main_productinfo(title, link, imageURL, price, maker, category1, category2) VALUES(?, ?, ?, ?, ?, ?, ?);"
+            
+            rows = []
+            for idx, row in info_df.iterrows():
+                rows.append(row)
+
+
+            cursor.executemany(sql, rows)
+            conn.commit()
+            conn.close()
 
     except:
         conn.rollback()
-        return Http404('Not Found Page!')
+        return redirect('main:index')
 
     return redirect('main:detail', keyword)
 
